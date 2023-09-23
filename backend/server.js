03f62168172ba826c67e3b7df8a0cd44f657a346
CalebeSG <calebeguimaraes10@gmail.com>
2023-09-23 17:30:16 -0300

    vercel config

--- backend/server.js
+++ backend/server.js
@@ -4,21 +4,21 @@ require("dotenv").config();
 
 const app = express();
 
-const allowCrossDomain = (req, res, next) => {
-  res.header(`Access-Control-Allow-Origin`, [
-    "https://cors-config-j1yd.vercel.app",
-    "*",
-  ]);
-  res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
-  res.header(`Access-Control-Allow-Headers`, `Content-Type`);
-  next();
-};
+// const allowCrossDomain = (req, res, next) => {
+//   res.header(`Access-Control-Allow-Origin`, [
+//     "https://cors-config-j1yd.vercel.app",
+//     "*",
+//   ]);
+//   res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
+//   res.header(`Access-Control-Allow-Headers`, `Content-Type`);
+//   next();
+// };
 
-// app.use(cors());
-app.use(allowCrossDomain);
+app.use(cors());
+// app.use(allowCrossDomain);
 app.use(express.json());
 
-app.get("/", allowCrossDomain, (req, res) => {
+app.get("/", (req, res) => {
   return res.json({ status: "ok" });
 });
 
