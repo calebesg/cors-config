100a07b16f5f35d5b0f1c45082648cc65616060e
CalebeSG <calebeguimaraes10@gmail.com>
2023-09-23 17:39:07 -0300

    sdfsf

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,26 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 
 const app = express();
 
-// const allowCrossDomain = (req, res, next) => {
-//   res.header(`Access-Control-Allow-Origin`, [
-//     "https://cors-config-j1yd.vercel.app",
-//     "*",
-//   ]);
-//   res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
-//   res.header(`Access-Control-Allow-Headers`, `Content-Type`);
-//   next();
-// };
+const allowCrossDomain = (req, res, next) => {
+  res.header(`Access-Control-Allow-Origin`, [
+    "https://cors-config-j1yd.vercel.app",
+    "*",
+  ]);
+  res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
+  res.header(`Access-Control-Allow-Headers`, `Content-Type`);
+  next();
+};
 
+app.use(allowCrossDomain);
 app.use(cors());
-// app.use(allowCrossDomain);
 app.use(express.json());
 
 app.get("/", (req, res) => {
   return res.json({ status: "ok" });
 });
 
-app.listen(process.env.PORT || 3333, console.log("APP RUNING"));
+const port = process.env.PORT || 3333;
+
+app.listen(port);
