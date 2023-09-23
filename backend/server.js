7a57dc3bb081f1180b2b10eb782eb865a6d30ab8
CalebeSG <calebeguimaraes10@gmail.com>
2023-09-23 16:39:45 -0300

    config 4

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,15 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
-app.use((req, res, next) => {
-  res.append("Access-Control-Allow-Origin", ["*"]);
-  res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.append("Access-Control-Allow-Headers", "Content-Type");
+const allowCrossDomain = (req, res, next) => {
+  res.header(`Access-Control-Allow-Origin`, `*`);
+  res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
+  res.header(`Access-Control-Allow-Headers`, `Content-Type`);
   next();
-});
+};
+
+app.use(cors());
+app.use(allowCrossDomain);
 app.use(express.json());
 
 app.get("/", (req, res) => {
